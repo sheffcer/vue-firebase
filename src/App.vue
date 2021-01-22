c186c83e340824adc12eebd165470a5e5bb3da90
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-22 16:52:26 +0200

    handling errors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <form class="card" @submit.prevent="createUser">
+      <form class="card" @submit.prevent="createUser">
+      <app-error :alert="alert" @close-alert="alert = null"></app-error>
       <h2>Post with FireBase</h2>
       <div class="form-control">
         <label for="name">Веедите имя</label>
@@ -19,12 +20,14 @@
 
 <script>
 import AppUserslList from './components/AppUsersList'
+import AppError from './components/AppError'
 import axios from 'axios'
 export default {
   data () {
     return {
       name: '',
-      users: []
+      users: [],
+      alert: null
     }
   },
   mounted () {
@@ -52,16 +55,25 @@ export default {
     },
     async loadUsers () {
       const url = 'https://vue-firebase-146dd-default-rtdb.europe-west1.firebasedatabase.app/users.json'
-      const { data } = await axios.get(url)
-      const result = Object.keys(data).map(key => {
-        return {
-          id: key,
-          firstName: data[key].firstName
+      try {
+        const { data } = await axios.get(url)
+        if (!data) {
+          throw new Error('Список юзеров пуст')
         }
-      })
-      this.users = result
-      // console.log(data)
-      // console.log(result)
+        this.users = Object.keys(data).map(key => {
+          return {
+            id: key,
+            firstName: data[key].firstName
+          }
+        })
+      } catch (error) {
+        this.alert = {
+          type: 'danger',
+          title: 'Ошибка ёпта',
+          text: error.message
+        }
+        console.log(error.message)
+      }
     },
     async removeUser (id) {
       await axios.delete(`https://vue-firebase-146dd-default-rtdb.europe-west1.firebasedatabase.app/users/${id}.json`)
@@ -69,7 +81,8 @@ export default {
     }
   },
   components: {
-    AppUserslList
+    AppUserslList,
+    AppError
   }
 }
 </script>
---
+++ src/components/AppError.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+  :v-model="alert"
+  v-if="alert"
+  :class="alert.type"
+  >
+  <h3>{{alert.title}}</h3>
+  <p>{{alert.text}}</p>
+  <button class="btn" :class="alert.type" @click="$emit('close-alert')">Закрыть</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close-alert'],
+  props: ['alert']
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
